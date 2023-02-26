ccf757e9269fd033060da024fc60449bea0243d4
Monir Hossain <mmonir.m.29@gmail.com>
2023-02-26 20:56:17 +0600

    Restrict total user number to one.

--- account/models.py
+++ account/models.py
@@ -1,9 +1,13 @@
 
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    def clean(self, *args, **kwargs):
+        if User.objects.all().exists():
+            raise ValidationError("Database only allow one user")
+        super().clean(*args, **kwargs)
 
 
 
