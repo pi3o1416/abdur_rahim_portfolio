47c941d36dc4d335b8542705276fdb6402092f05
Monir Hossain <mmonir.m.29@gmail.com>
2023-02-28 00:33:27 +0600

    Add STATIC_ROOT and STATICFILES_DIRS to settings.

--- ARPortfolio/settings.py
+++ ARPortfolio/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'ARPortfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
